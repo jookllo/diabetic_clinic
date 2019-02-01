cd8766e33e5ae164e6932de7d7593d28e50350ed
jookllo <hokello34@gmail.com>
2019-02-01 16:52:36 +0300

    Added update and edit tab

--- pages/navbar.php
+++ pages/navbar.php
@@ -18,21 +18,26 @@
             <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
-                        <li class="sidebar-search">
-                            <div class="input-group custom-search-form">
-                                <input type="text" class="form-control" placeholder="Search...">
-                                <span class="input-group-btn">
-                                    <button class="btn btn-default" type="button">
-                                        <i class="fa fa-search"></i>
-                                    </button>
-                                </span>
-                            </div>
-                            <!-- /input-group -->
-                        </li>
-                        <li>
-                            <a href="home.php"><i class="fa fa-edit fa-fw"></i> Patient Details</a>
-                        </li>
+                       
                         
+                        
+                        <li>
+                        <a href="#"><i class="fa fa-sitemap fa-fw"></i> Patient Details<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="home.php">Add New Patient</a>
+                                </li>
+                                <li>
+                                    <a href="update.php">Delete or Edit Patient Records </a>
+                                </li>
+                                <li>
+                                    
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        </li>
+
                         <li><a href="analysis.php"><i class="fa fa-edit fa-fw"></i> Patient Analysis</a>
                            
                         </li>
@@ -47,7 +52,6 @@
                         <li>
                             <a href="forms.php"><i class="fa fa-table fa-fw"></i> Reports and Analysis</a>
                         </li>
-                        
                         
                        
                     </ul>
