6b3431a73a13d181e52d7794522a1af9643edcc5
jookllo <hokello34@gmail.com>
2019-08-08 18:03:10 +0300

    Removed the none needed files

--- hideen_files/addpatient.js
+++
@@ -1,9 +0,0 @@
-$("add_patient").click(function(){
-  $.post($("#add-patient").attr("action"), $("#add-patient :input").serializeArray(),
-   function(info){$("#result").html(info);})
-    });
-
-  $("#add-patient").submit(function(){
-    return false;
-  });
-
